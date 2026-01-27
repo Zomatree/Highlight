23ffbfafbdf8378201e6d6671332e21c315d7734
Zomatree <me@zomatree.live>
2026-01-27 04:02:42 +0000

    switch to crates.io deps

--- crates/highlight/src/commands/mod.rs
+++ crates/highlight/src/commands/mod.rs
@@ -50,10 +50,208 @@ impl CommandEventHandler for CommandEvents {
     }
 }
 
+// async fn play(ctx: Context<Error, State>) -> Result<(), Error> {
+//     let Some((voice_channel_id, _)) = ctx.get_member().await?.voice(&ctx) else {
+//         return Ok(());
+//     };
+
+//     let channel = ctx.cache.get_channel(&voice_channel_id).unwrap();
+
+//     let conn = channel
+//         .join_call(
+//             &ctx,
+//             &ctx,
+//             Some(ctx.cache.livekit_nodes().first().unwrap().name.clone()),
+//         )
+//         .await?;
+
+//     // let (a, b) = async_ringbuf::AsyncHeapRb::new(1920 * 5).split();
+
+//     // let sink = RingSink { prod: Mutex::new(a) };
+//     // let source = RingSource::new(Mutex::new(b));
+
+//     // let (_, _, p) = conn.remote_participants().into_iter().find(|(user, _, p)| &user.id == &ctx.message.author).unwrap();
+
+//     // let track = p.track_publications().into_values().find(|t| t.kind() == TrackKind::Audio).unwrap();
+
+//     // // pin!(sink);
+//     // // pin!(source);
+
+//     // select! {
+//     //     e = conn.listen_to_track(track.clone(), p.clone(), sink) => e,
+//     //     e = conn.play(source) => e,
+//     // }?;
+
+//     conn.play(RNNoiseFFmpegPCMAudio::new("audio.mp3")).await?;
+
+//     Ok(())
+// }
+
+// pub struct RNNoiseFFmpegPCMAudio {
+//     child: Child,
+//     stdout: ChildStdout,
+//     denoise: Box<DenoiseState<'static>>,
+//     buf: Vec<f32>,
+//     output: Vec<f32>,
+// }
+
+// impl RNNoiseFFmpegPCMAudio {
+//     pub fn new(source: &str) -> Self {
+//         let denoise = DenoiseState::new();
+
+//         let mut command = tokio::process::Command::new("ffmpeg");
+
+//         let mut child = command
+//             .arg("-i")
+//             .arg(source)
+//             .arg("-f")
+//             .arg("s16le")
+//             .arg("-acodec")
+//             .arg("pcm_s16le")
+//             .arg("-ar")
+//             .arg("48000")
+//             .arg("-ac")
+//             .arg("1")
+//             .arg("-vn")
+//             .arg("-loglevel")
+//             .arg("warning")
+//             .arg("-blocksize")
+//             .arg("8192")
+//             .arg("pipe:1")
+//             .stdout(Stdio::piped())
+//             .spawn()
+//             .unwrap();
+
+//         let stdout = child.stdout.take().unwrap();
+
+//         Self {
+//             child,
+//             stdout,
+//             denoise,
+//             buf: vec![0.; 4 * FRAME_SIZE],
+//             output: vec![0.; 4 * FRAME_SIZE],
+//         }
+//     }
+// }
+
+// #[async_trait]
+// impl AudioSource for RNNoiseFFmpegPCMAudio {
+//     fn channels(&self) -> usize {
+//         1
+//     }
+//     fn frame_length_ms(&self) -> usize {
+//         20
+//     }
+
+//     async fn read(&mut self, buffer: &mut [i16]) -> bool {
+//         for i in 0..self.frame_size() {
+//             if let Ok(data) = self.stdout.read_i16_le().await {
+//                 self.buf[i] = data as f32;
+//             } else {
+//                 return true;
+//             };
+//         }
+
+//         for i in 0..(self.frame_length_ms() / 5) {
+//             let lower = i * FRAME_SIZE;
+//             let upper = lower + FRAME_SIZE;
+
+//             self.denoise
+//                 .process_frame(&mut self.output[lower..upper], &self.buf[lower..upper]);
+//         }
+
+//         for i in 0..self.frame_size() {
+//             buffer[i] = self.output[i] as i16;
+//         }
+
+//         false
+//     }
+
+//     async fn close(mut self) {
+//         let _ = self.child.kill().await;
+//     }
+// }
+
+// struct RingSink {
+//     pub prod: Mutex<AsyncHeapProd<i16>>,
+// }
+
+// #[async_trait]
+// impl AudioSink for RingSink {
+//     fn channels(&self) -> usize {
+//         1
+//     }
+
+//     async fn sink(
+//         &mut self,
+//         _participant: RemoteParticipant,
+//         _track: RemoteAudioTrack,
+//         frame: AudioFrame<'_>,
+//     ) {
+//         self.prod.lock().await.push_slice(&frame.data);
+//     }
+// }
+
+// struct RingSource {
+//     pub cons: Mutex<AsyncHeapCons<i16>>,
+//     denoise: Box<DenoiseState<'static>>,
+//     buf: Vec<f32>,
+//     output: Vec<f32>,
+// }
+
+// impl RingSource {
+//     pub fn new(cons: Mutex<AsyncHeapCons<i16>>) -> Self {
+//         Self {
+//             cons,
+//             denoise: DenoiseState::new(),
+//             buf: vec![0.; 4 * FRAME_SIZE],
+//             output: vec![0.; 4 * FRAME_SIZE],
+//         }
+//     }
+// }
+
+// #[async_trait]
+// impl AudioSource for RingSource {
+//     fn channels(&self) -> usize {
+//         1
+//     }
+
+//     async fn read(&mut self, buffer: &mut [i16]) -> bool {
+//         let mut cons = self.cons.lock().await;
+
+//         for i in 0..self.frame_size() {
+//             self.buf[i] = cons.next().await.unwrap_or_default() as f32;
+//         }
+
+//         for i in 0..(self.frame_length_ms() / 5) {
+//             let lower = i * FRAME_SIZE;
+//             let upper = lower + FRAME_SIZE;
+
+//             self.denoise
+//                 .process_frame(&mut self.output[lower..upper], &self.buf[lower..upper]);
+//         }
+
+//         for i in 0..self.frame_size() {
+//             buffer[i] = self.output[i] as i16;
+//         }
+
+//         false
+//     }
+// }
+
+// async fn close(ctx: Context<Error, State>) -> Result<(), Error> {
+//     ctx.events.close()?;
+
+//     Ok(())
+// }
 
 pub fn commands() -> Vec<Command<Error, State>> {
     vec![
         highlight::command(),
         info::command(),
+        // Command::new("play", play).description("audio"),
+        // Command::new("close", close)
+        //     .description("Turns off the bot")
+        //     .check(is_owner),
     ]
 }
